5d3332ff34591fdabf77a27bbbb8c0a68f3bfe49
idan <idan.b@heilasystems.com>
2023-02-20 09:42:46 +0200

    fix orchest start \ stop

--- orchestD.sh
+++ orchestD.sh
@@ -3,10 +3,9 @@
 docker-compose -f integrations/docker-compose.yml $1
 docker-compose -f docker-compose-orchestd.yml $1
 
-cd ~/orchestD/bin
-
 if [ ${1} == 'start' ]; then
-nohup ./orchestD &
+  cd ~/orchestD/bin
+  nohup ./orchestD &
   orchestDUrl=http://127.0.0.1:29000/
   xdg-open $orchestDUrl
 fi
