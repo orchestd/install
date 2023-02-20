d7a0cc8c29e23af44a2eb4574935bc9ea4384bce
idan <idan.b@heilasystems.com>
2023-02-20 14:04:12 +0200

    fix-cp-install

--- installOrchestDDocker.sh
+++ installOrchestDDocker.sh
@@ -143,9 +143,9 @@ cd $origpath
 
 # copy install folder
 mkdir $userPath/bin/install
-cp -r docker-compose-orchestd.yml $userPath/bin/install
-cp -r orchestd.sh $userPath/bin/install
-cp -r integrations $userPath/bin/install
+cp -r docker-compose-orchestd.yml $userPath/bin
+cp -r orchestd.sh $userPath/bin
+cp -r integrations $userPath/bin/integrations
 
 show "###  docker-compose run orchestD  ###"
 docker-compose -f docker-compose-orchestd.yml up -d
