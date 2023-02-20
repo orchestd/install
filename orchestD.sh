d389eed87c1bc6474a7d29b61e23864927152f79
idan <idan.b@heilasystems.com>
2023-02-20 09:56:25 +0200

    exports

--- orchestD.sh
+++ orchestD.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
+export ORCHESTD_REGISTRY=eu.gcr.io/orchestd-io
+export HEILA_TYPE=HEAD
+export HEILA_ENV=LOCAL
+export GIN_MODE=release
+export TAG=lastmerge
+export DOCKER_NAME=orchestD
+
 if [ ${1} == 'start' ]; then
+  docker-compose -f integrations/docker-compose.yml $1
+  docker-compose -f docker-compose-orchestd.yml $1
   cd ~/orchestD/bin
   nohup ./orchestD &
   orchestDUrl=http://127.0.0.1:29000/
   xdg-open $orchestDUrl
 elif [ ${1} == 'stop' ]; then
+  docker-compose -f integrations/docker-compose.yml $1
+  docker-compose -f docker-compose-orchestd.yml $1
   killall -e orchestD
 else
   echo "Usage: ./orchestD [start] OR [stop]"
   exit
 fi
-
-docker-compose -f integrations/docker-compose.yml $1
-docker-compose -f docker-compose-orchestd.yml $1
