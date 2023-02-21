e14b8564374c1fbde2c2b96181d4e88d3c7f8212
orchestd <123419555+orchestd@users.noreply.github.com>
2023-02-21 12:21:10 +0200

    add update action (#4)
    * add update action
    
    add update action
    
    * Update orchestd.sh
    
    * Update orchestd.sh
    
    * Update orchestd.sh
    
    * Update orchestd.sh

--- orchestd.sh
+++ orchestd.sh
@@ -9,17 +9,34 @@ export GIN_MODE=release
 export TAG=lastmerge
 export DOCKER_NAME=orchestD
 
-if [ ${1} == 'start' ]; then
+if [[ ${1} == 'start' ]]; then
   docker-compose -f integrations/docker-compose.yml $1
   docker-compose -f docker-compose-orchestd.yml $1
   nohup ./orchestD &
   orchestDUrl=http://127.0.0.1:29000/
   xdg-open $orchestDUrl
-elif [ ${1} == 'stop' ]; then
+elif [[ ${1} == 'stop' ]]; then
   docker-compose -f integrations/docker-compose.yml $1
   docker-compose -f docker-compose-orchestd.yml $1
   killall -e orchestD
+elif [[ ${1} == 'update' ]]; then
+  export TAG=latest
+
+DigestCurrent=$(docker images --digests eu.gcr.io/orchestd-io/servicebuilder --format "{{.Digest}} {{.Tag}}" | grep latest | awk  '{print $1}')
+
+docker-compose -f docker-compose-orchestd.yml pull
+
+DigestNew=$(docker images --digests eu.gcr.io/orchestd-io/servicebuilder --format "{{.Digest}} {{.Tag}}" | grep latest | awk  '{print $1}')
+
+if [[ ${DigestCurrent} != ${DigestNew} ]]; then
+  docker-compose -f docker-compose-orchestd.yml stop
+  docker-compose -f docker-compose-orchestd.yml up -d
+  echo "New version updated !"
 else
-  echo "Usage: ./orchestD [start] OR [stop]"
+    echo "Nothing changed"
+fi
+
+else
+  echo "Usage: ./orchestD [start] OR [stop] OR [update]"
   exit
 fi
